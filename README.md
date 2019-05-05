4deeb307d9b4b8a3f4e1141afa30bce5c98feed9
m16averick <298282@uwr.edu.pl>
2019-05-05 19:06:53 +0200

    init
    pre alpha relase

---
+++ README.md
@@ -0,0 +1,8 @@
+# HTTP WScan
+
+Hi! I'm Mike from Poland and this is my open source Python script which allows you to scan range of WEB to find working http websites. 
+
+
+## Usage
+
+To use this Class just type **Scan(address, and mask)**
---
+++ scan.py
@@ -0,0 +1,117 @@
+import requests as req
+
+
+class Scan:
+    def __init__(self, ip, maskOCT):
+        print("About scanned network: \n")
+        ipv4 = ip.split(".")
+        octet_list_bin = [format(int(i), '08b') for i in ipv4]
+
+        binary = ("").join(octet_list_bin)
+        octet_list_bin = list(binary)
+        print("Address: " + str(self.obs(binary)))
+
+        maskLIST=[]
+        negativeLIST=[]
+
+        for i in range(32):
+          if i < maskOCT:
+            maskLIST.append(str(1))
+            negativeLIST.append(str(0))
+          else:
+            maskLIST.append(str(0))
+            negativeLIST.append(str(1))
+        maskBIN = ("").join(maskLIST)
+        negativeBIN = ("").join(negativeLIST)
+
+        print("Mask: " + str(self.obs(maskBIN)))
+
+        gatewayBIN = []
+
+        for i in range (len(octet_list_bin)):
+            if (maskLIST[i] == "1"):
+                gatewayBIN.append(octet_list_bin[i])
+            else:
+                gatewayBIN.append(maskLIST[i])
+
+        gatewayBIN = ("").join(gatewayBIN[0:32])
+        gatewayDEC = self.obs( gatewayBIN )
+
+        broadcastBIN =  (bin(int(gatewayBIN, 2) + int(negativeBIN, 2))) [2:]
+        gatewayBIN =  (bin(int(gatewayBIN, 2) + int("1", 2))) [2:]
+        print("Gateway: " + str(self.obs(gatewayBIN)))
+        print("Broadcast: " + str(self.obs(broadcastBIN)))
+
+        broadcastBIN =  (bin(int(gatewayBIN, 2) + int("100000000000", 2))) [2:]
+
+
+
+        print("\n")
+
+        self.start(gatewayBIN, broadcastBIN)
+        print("That's all, n-joy ;)")
+        #print(self.server(url))
+
+        #self.url = url
+
+
+    def start(self, gatewayBIN, broadcastBIN):
+        addressBIN = gatewayBIN
+        while ( bin(int(addressBIN,2 )) < bin(int(broadcastBIN, 2))):
+            addressBIN = (bin(int(addressBIN, 2) + int("1", 2)))[2:]
+            try:
+                addresDEC = self.obs(addressBIN)
+                code = (req.head(("http://"+addresDEC), timeout=0.1).status_code)
+                if (code<400):
+                    print("")
+                    try:
+                        print("Code: ", code)
+                        print("DNS Address: " + self.address("http://" + addresDEC))
+                        print("Title: " + self.name("http://"+addresDEC))
+                        print("Server: " + self.server("http://"+addresDEC))
+                    except:
+                        try:
+                            print("Address: " + addresDEC)
+                            print("Title: " + self.name("http://" + addresDEC))
+                            print("Server: " + self.server("http://" + addresDEC))
+                        except:
+                              try:
+                                print("Title: " + self.name("http://" + addresDEC))
+                                print("Server: " + self.server("http://" + addresDEC))
+                              except:
+                                  continue
+
+                    #print(self.server("http://"+addresDEC))
+            except req.exceptions.RequestException:
+                #print("Nie ma takiego serwera")
+                continue
+            #print(str(self.obs(addressBIN)))
+
+
+    def obs(self, long): #OctoBinarySeperator xD
+        short = [ int(("").join(long[0:8]),2), int(("").join(long[8:16]),2), int(("").join(long[16:24]),2), int(("").join(long[24:32]),2) ]
+        short = (str(short[0])+"."+str(short[1])+"."+str(short[2])+"."+str(short[3]))
+        return(short)
+
+    def name(self, url):
+        resp = req.head(url)
+        n = req.get(url, resp.headers)
+        name1 = n.text
+        namez = name1[name1.find('<title>') + 7: name1.find('</title>')]
+        if (len(namez) > 100):
+            return ("None")
+        else:
+            return(namez)
+
+    def address(self, url):
+        resp = req.head(url)
+        address = str(resp.headers['Location'])
+        return(address)
+
+    def server(self, url):
+        resp = req.head(url)
+        server = str(resp.headers['server'])
+        return(server)
+
+
+Scan("156.17.88.0", 24)
